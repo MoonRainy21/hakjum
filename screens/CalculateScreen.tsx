cc3cdee5c7a816769fd610f308d5a8d46a52062c
MoonRainy21 <shmoon232@gmail.com>
2022-01-13 16:01:56 +0900

    Now we can make table with Divider

--- screens/CalculateScreen.tsx
+++ screens/CalculateScreen.tsx
@@ -1,9 +1,25 @@
-import { Center } from "native-base";
+import { Center, HStack, VStack, Text, Divider, Flex, Heading } from "native-base";
 
 export default function CalculateScreen() {
     return (
-        <Center>
-            Calculate The Hakjum
+        <Center
+            bg='white'
+            px={4}
+            flex={1}
+        >
+            <VStack space={5} alignItems='center'>
+                <Flex mx='10' direction='row' justify='space-evenly'>
+                    <Heading>Hi</Heading>
+                    <Divider mx="10" orientation="vertical" thickness='0'/>
+                    <Heading>Hi</Heading>
+                </Flex>
+                <Divider my='2'/>
+                <HStack>
+                    <Text>dfhwgiuwholdjf</Text>
+                    <Text>Hello</Text>
+                    <Text>Please</Text>
+                </HStack>
+            </VStack>
         </Center>
     )
 }
